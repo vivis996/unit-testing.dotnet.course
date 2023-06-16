c749cc0874e4d5455a13d2f9da3552e9acdc248b
Daniel Viveros <dviveros90@gmail.com>
2023-06-15 20:44:11 -0400

    Writing your first unit test

---
+++ TestNinja.Tests/ReservationTests.cs
@@ -0,0 +1,20 @@
+﻿using TestNinja.Fundamentals;
+
+namespace TestNinja.Tests;
+
+[TestClass]
+public class ReservationTests
+{
+    [TestMethod]
+    public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+    {
+        // Arrange
+        var reservation = new Reservation();
+
+        // Act
+        var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+}
