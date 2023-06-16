c8e591d9b7b52aada8fc41fa9f5f8d405295015b
Daniel Viveros <dviveros90@gmail.com>
2023-06-15 22:15:56 -0400

    Black-box Testing

--- TestNinja.Tests/MathTests.cs
+++ TestNinja.Tests/MathTests.cs
@@ -14,4 +14,34 @@ public class MathTests
 
         Assert.That(result, Is.EqualTo(3));
     }
+
+    [Test]
+    public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+    {
+        var math = new Math();
+
+        var result = math.Max(2, 1);
+
+        Assert.That(result, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
+    {
+        var math = new Math();
+
+        var result = math.Max(1, 2);
+
+        Assert.That(result, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
+    {
+        var math = new Math();
+
+        var result = math.Max(1, 1);
+
+        Assert.That(result, Is.EqualTo(1));
+    }
 }
