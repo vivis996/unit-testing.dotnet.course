f6413cf8ed5a9456353b0c46092407f131f784cc
Daniel Viveros <dviveros90@gmail.com>
2023-06-15 22:09:44 -0400

    What to Test and What Not to Test

---
+++ TestNinja.Tests/MathTests.cs
@@ -0,0 +1,17 @@
+﻿using Math = TestNinja.Fundamentals.Math;
+
+namespace TestNinja.Tests;
+
+[TestFixture]
+public class MathTests
+{
+    [Test]
+    public void Add_WhenCalled_ReturnTheSumsOfArguments()
+    {
+        var math = new Math();
+
+        var result = math.Add(1, 2);
+
+        Assert.That(result, Is.EqualTo(3));
+    }
+}
