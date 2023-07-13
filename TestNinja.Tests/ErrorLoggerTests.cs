a03bf2a8107ebe6a9c3a75d153fd344b71a96484
Daniel Viveros <dviveros90@gmail.com>
2023-07-13 15:49:08 -0400

    Testing Void Methods

---
+++ TestNinja.Tests/ErrorLoggerTests.cs
@@ -0,0 +1,24 @@
+﻿using TestNinja.Fundamentals;
+
+namespace TestNinja.Tests;
+
+[TestFixture]
+public class ErrorLoggerTests
+{
+    private ErrorLogger _logger;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this._logger = new ErrorLogger();
+    }
+
+    [Test]
+    public void Log_WhenCalled_SetTheLastErrorProperty()
+    {
+        _logger.Log("a");
+
+        Assert.That(_logger.LastError, Is.EqualTo("a"));
+    }
+}
+
