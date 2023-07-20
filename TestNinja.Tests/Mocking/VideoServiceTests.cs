f425c43ca7807242c5ed98d0205279b696e3645a
Daniel Viveros <dviveros90@gmail.com>
2023-07-20 19:01:43 -0400

    Dependency Injection via Method Parameters

---
+++ TestNinja.Tests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TestNinja.Mocking;
+
+namespace TestNinja.Tests.Mocking;
+
+[TestFixture]
+public class VideoServiceTests
+{
+    private VideoService _service;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this._service = new VideoService();
+    }
+
+    [Test]
+    public void ReadVideoTitle_EmptyFile_ReturnError()
+    {
+        var result = this._service.ReadVideoTitle(new FakeFileReader());
+
+        Assert.That(result, Does.Contain("error").IgnoreCase);
+    }
+}
