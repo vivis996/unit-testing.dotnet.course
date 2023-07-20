35096ece4e93f90542b4b21a2f41ef169d0c79d9
Daniel Viveros <dviveros90@gmail.com>
2023-07-20 18:38:39 -0400

    Refactoring Towards a Lossely-coupled Design

---
+++ TestNinja.Tests/FakeFileReader.cs
@@ -0,0 +1,12 @@
+﻿using TestNinja.Mocking;
+
+namespace TestNinja.Tests;
+
+public class FakeFileReader : IFileReader
+{
+    public string Read(string path)
+    {
+        return "";
+    }
+}
+
---
+++ TestNinja/Mocking/FileReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestNinja.Mocking;
+
+public interface IFileReader
+{
+    string Read(string path);
+}
+
+public class FileReader : IFileReader
+{
+	public string Read(string path)
+	{
+        return File.ReadAllText(path);
+    }
+}
