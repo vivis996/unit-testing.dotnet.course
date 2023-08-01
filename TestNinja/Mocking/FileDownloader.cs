54d60fec6d41ce25f4167ce6ac9b51c82737461a
Daniel Viveros <dviveros90@gmail.com>
2023-08-01 19:42:58 -0400

    Refactoring InstallerHelper

---
+++ TestNinja/Mocking/FileDownloader.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace TestNinja.Mocking;
+
+public class FileDownloader : IFileDownloader
+{
+    public void DownloadFile(string url, string path)
+    {
+        var client = new WebClient();
+        client.DownloadFile(url, path);
+    }
+}
+
+public interface IFileDownloader
+{
+    void DownloadFile(string url, string path);
+}
--- TestNinja/Mocking/InstallerHelper.cs
+++ TestNinja/Mocking/InstallerHelper.cs
@@ -1,28 +1,32 @@
 ﻿using System.Net;
 
-namespace TestNinja.Mocking
+namespace TestNinja.Mocking;
+
+public class InstallerHelper
 {
-    public class InstallerHelper
+    private string _setupDestinationFile;
+    private IFileDownloader _fileDownloader;
+
+    public InstallerHelper(IFileDownloader fileDownloader)
     {
-        private string _setupDestinationFile;
+        this._fileDownloader = fileDownloader;
+    }
 
-        public bool DownloadInstaller(string customerName, string installerName)
+    public bool DownloadInstaller(string customerName, string installerName)
+    {
+        try
         {
-            var client = new WebClient();
-            try
-            {
-                client.DownloadFile(
-                    string.Format("http://example.com/{0}/{1}",
-                        customerName,
-                        installerName),
-                    _setupDestinationFile);
+            this._fileDownloader.DownloadFile(
+                string.Format("http://example.com/{0}/{1}",
+                    customerName,
+                    installerName),
+                _setupDestinationFile);
 
-                return true;
-            }
-            catch (WebException)
-            {
-                return false; 
-            }
+            return true;
+        }
+        catch (WebException)
+        {
+            return false; 
         }
     }
 }
