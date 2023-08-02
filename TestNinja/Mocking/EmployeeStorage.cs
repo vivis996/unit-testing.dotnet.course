1f49c3e86316a744c3f6110f0f8ad1dd31de784c
Daniel Viveros <dviveros90@gmail.com>
2023-08-02 11:59:41 -0400

    Refactoring EmployeeController

---
+++ TestNinja/Mocking/EmployeeStorage.cs
@@ -0,0 +1,26 @@
+﻿namespace TestNinja.Mocking;
+
+public class EmployeeStorage : IEmployeeStorage
+{
+    private EmployeeContext _db;
+
+    public EmployeeStorage()
+    {
+        this._db = new EmployeeContext();
+    }
+
+    public void DeleteEmployee(int id)
+    {
+        var employee = this._db.Employees.Find(id);
+        if (employee == null)
+            return;
+
+        this._db.Employees.Remove(employee);
+        this._db.SaveChanges();
+    }
+}
+
+public interface IEmployeeStorage
+{
+    void DeleteEmployee(int id);
+}
