03418f69874a940e870aded52d2c3ed0413071eb
Daniel Viveros <dviveros90@gmail.com>
2023-08-02 12:06:31 -0400

    Testing EmployeeController

---
+++ TestNinja.Tests/Mocking/EmployeeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Moq;
+using TestNinja.Mocking;
+
+namespace TestNinja.Tests.Mocking;
+
+[TestFixture]
+public class EmployeeControllerTests
+{
+    [Test]
+    public void DeleteEmployee_WhenCalled_DeleteTheEmployeeFromDb()
+    {
+        var storage = new Mock<IEmployeeStorage>();
+        var controller = new EmployeeController(storage.Object);
+
+        controller.DeleteEmployee(1);
+
+        storage.Verify(s => s.DeleteEmployee(1));
+    }
+}
