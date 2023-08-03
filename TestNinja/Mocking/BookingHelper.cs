a21c806faa98f867c633e2ed4dfd1042dd341909
Daniel Viveros <dviveros90@gmail.com>
2023-08-03 12:01:36 -0400

    Extracting IBooking Repository

--- TestNinja/Mocking/BookingHelper.cs
+++ TestNinja/Mocking/BookingHelper.cs
@@ -2,47 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace TestNinja.Mocking
+namespace TestNinja.Mocking;
+
+public static class BookingHelper
 {
-    public static class BookingHelper
+    public static string OverlappingBookingsExist(Booking booking, IBookingRepository repository)
     {
-        public static string OverlappingBookingsExist(Booking booking)
-        {
-            if (booking.Status == "Cancelled")
-                return string.Empty;
+        if (booking.Status == "Cancelled")
+            return string.Empty;
 
-            var unitOfWork = new UnitOfWork();
-            var bookings =
-                unitOfWork.Query<Booking>()
-                    .Where(
-                        b => b.Id != booking.Id && b.Status != "Cancelled");
+        var bookings = repository.GetActiveBookings(booking.Id);
 
-            var overlappingBooking =
-                bookings.FirstOrDefault(
-                    b =>
-                        booking.ArrivalDate >= b.ArrivalDate
-                        && booking.ArrivalDate < b.DepartureDate
-                        || booking.DepartureDate > b.ArrivalDate
-                        && booking.DepartureDate <= b.DepartureDate);
+        var overlappingBooking =
+            bookings.FirstOrDefault(
+                b =>
+                    booking.ArrivalDate >= b.ArrivalDate
+                    && booking.ArrivalDate < b.DepartureDate
+                    || booking.DepartureDate > b.ArrivalDate
+                    && booking.DepartureDate <= b.DepartureDate);
 
-            return overlappingBooking == null ? string.Empty : overlappingBooking.Reference;
-        }
+        return overlappingBooking == null ? string.Empty : overlappingBooking.Reference;
     }
+}
 
-    public class UnitOfWork
+public class UnitOfWork
+{
+    public IQueryable<T> Query<T>()
     {
-        public IQueryable<T> Query<T>()
-        {
-            return new List<T>().AsQueryable();
-        }
+        return new List<T>().AsQueryable();
     }
+}
 
-    public class Booking
-    {
-        public string Status { get; set; }
-        public int Id { get; set; }
-        public DateTime ArrivalDate { get; set; }
-        public DateTime DepartureDate { get; set; }
-        public string Reference { get; set; }
-    }
+public class Booking
+{
+    public string Status { get; set; }
+    public int Id { get; set; }
+    public DateTime ArrivalDate { get; set; }
+    public DateTime DepartureDate { get; set; }
+    public string Reference { get; set; }
 }
---
+++ TestNinja/Mocking/BookingRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TestNinja.Mocking;
+
+public class BookingRepository : IBookingRepository
+{
+    public IQueryable<Booking> GetActiveBookings(int? excludedBookingId = null)
+    {
+        var unitOfWork = new UnitOfWork();
+        var bookings =
+            unitOfWork.Query<Booking>()
+                .Where(b => b.Status != "Cancelled");
+
+        if (excludedBookingId.HasValue)
+            bookings = bookings.Where(b => b.Id != excludedBookingId);
+
+        return bookings;
+    }
+}
+
+public interface IBookingRepository
+{
+    IQueryable<Booking> GetActiveBookings(int? excludedBookingId = null);
+}
