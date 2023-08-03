fbf492b1f4f805b981b64883d3716f38196862b2
Daniel Viveros <dviveros90@gmail.com>
2023-08-03 11:38:09 -0400

    Tests Cases

---
+++ TestNinja.Tests/Mocking/BookingHelperTests.cs
@@ -0,0 +1,12 @@
+﻿namespace TestNinja.Tests.Mocking;
+
+[TestFixture]
+public class Booking_OverlappingBookingsExistHelperTests
+{
+    [Test]
+    public void BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
+    {
+
+    }
+}
+
